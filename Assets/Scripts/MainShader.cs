282b1bd02e94048878ba1de2fb64ec9746f4a76b
Jonas Svensson <jonas.m.svensson@gmail.com>
2016-12-11 11:37:39 +0100

    Added shaders

---
+++ Assets/Scripts/MainShader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainShader : MonoBehaviour {
+
+	public Material mat;
+	public Shader shader;
+	void Start() {
+		mat = new Material(shader);
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		Graphics.Blit(src, dest, mat);
+	}
+}
