a2fbee3b5477c8744114fddf96be518e6eb618df
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-12-17 00:25:28 +0100

    :star: Testing making diorama

---
+++ Assets/Scripts/Diorama.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diorama : MonoBehaviour {
+
+    [SerializeField]
+    Camera mainCam;
+
+    [SerializeField]
+    Transform referenceWorldPosition;
+
+    [SerializeField]
+    Camera dioramaCam;
+
+    [SerializeField]
+    Transform dioramaReferenecPosition;
+    
+    [SerializeField]
+    Transform screen;
+
+    [SerializeField]
+    Transform sceneFocus;
+
+	void Update () {
+        TranslateDioramaCam();
+        dioramaCam.transform.rotation = mainCam.transform.rotation;
+        dioramaCam.fieldOfView = mainCam.fieldOfView;
+	}
+
+
+    void TranslateDioramaCam()
+    {
+        dioramaCam.transform.position = mainCam.transform.position - referenceWorldPosition.position + dioramaReferenecPosition.position;
+    }
+
+}
