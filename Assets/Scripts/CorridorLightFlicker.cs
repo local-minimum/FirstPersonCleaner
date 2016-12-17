92d0a2badd30fc84d0ef2fced2b27f295109c026
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-12-17 18:08:51 +0100

    :wrench: Flicker light turns off emission too

--- Assets/Scripts/CorridorLightFlicker.cs
+++ Assets/Scripts/CorridorLightFlicker.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class CorridorLightFlicker : MonoBehaviour {
 
+    [SerializeField]
     Light myLight;
-    float onIntencity;
-    
-	void Start () {
-        myLight = GetComponent<Light>();
-        onIntencity = myLight.intensity;
 
+    Material lightMat;
+
+    float onIntencity;
+
+    Color onEmission;
+    Color onColor;
+
+    [SerializeField]
+    float offColorFactor = 0.5f;
+
+    [SerializeField]
+    float offEmissionFactor = 0.1f;
+
+	void Start () {
+
+        if (myLight == null)
+        {
+            myLight = GetComponent<Light>();
+        }
+
+        lightMat = GetComponent<MeshRenderer>().material;
+
+        onColor = lightMat.GetColor("_Color");
+        onEmission = lightMat.GetColor("_EmissionColor");
+        onIntencity = myLight.intensity;
         StartCoroutine(flicker());
+
 	}
 	
 	IEnumerator<WaitForSeconds> flicker()
@@ -21,7 +43,9 @@ public class CorridorLightFlicker : MonoBehaviour {
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(0.2f, 1));
-            myLight.intensity = isOn ? 0 : onIntencity;
+            myLight.intensity = isOn ? onIntencity : 0;
+            lightMat.SetColor("_Color", onColor * (isOn ? 1 : offColorFactor));
+            lightMat.SetColor("_EmissionColor", onEmission * (isOn ? 1 : offEmissionFactor));
             isOn = !isOn;
         }
     }
